ae4561734348e7ff45c47b4b28787b3b822318c3
juno-junho <ssmm0205@naver.com>
2024-05-22 19:54:52 +0900

    선착순 동시성 문제 2. Redis의 incr 명령어를 통한 선착순 문제 해결

--- src/main/java/com/junho/coupon/service/ApplyService.java
+++ src/main/java/com/junho/coupon/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.junho.coupon.service;
 
 import com.junho.coupon.domain.Coupon;
+import com.junho.coupon.repository.CouponCountRepository;
 import com.junho.coupon.repository.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class ApplyService {
 
     private final CouponRepository couponRepository;
+    private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     /**
@@ -20,7 +23,8 @@ public class ApplyService {
      */
     public void apply(Long userId) {
         // coupon 조회
-        long count = couponRepository.count();
+//        long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
 
         // 100개 이상 발급하지 않도록 제한
         if (count > 100) {
